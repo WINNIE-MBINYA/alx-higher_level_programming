6c8c9173aa92f4470d0c307b46c4dc78bf519aa9
WINNIE-MBINYA <winnymbinyamwanza@gmail.com>
2023-12-01 20:46:08 +0300

    Whitespace

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,10 +3,10 @@ from sys import argv
 
 if __name__ == "__main__":
     argc = len(argv) - 1  # Number of arguments (excluding script name)
-    
+
     print("{} argument{}{}".format(
         argc, 's' if argc != 1 else '', ':' if argc > 0 else '.'
     ))
-    
+
     for i, arg in enumerate(argv[1:], start=1):
         print("{}: {}".format(i, arg))
