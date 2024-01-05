cae406c5fa9318b5db38d3e96c1bae23e8012939
WINNIE-MBINYA <winnymbinyamwanza@gmail.com>
2024-01-05 20:29:26 +0300

    Memory cost

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
-class LockedClass:
-    """
-    A class that prevents the dynamic creation of new instance attributes,
-    except for 'first_name'.
-    """
-    __slots__ = ('first_name',)
+class LockedClass():
+    """A class that prevents the dynamic creation of new instance attributes"""
+    __slots__ = ['first_name']
 
     def __init__(self):
-        """
-        Initializes an instance of LockedClass.
-        """
+        """Initializes an instance of LockedClass"""
         pass
