ec8ea5fb7d8d529faada8e5ea53794a6817d3112
WINNIE-MBINYA <winnymbinyamwanza@gmail.com>
2024-03-14 18:15:54 +0300

    Fix code

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,20 +1,17 @@
 #!/usr/bin/node
 const myObject = {
   type: 'object',
-  value: 12,
+  value: 12
+};
 
-  incr: () => {
-    this.value++;
-  }
+myObject.incr = function incr() {
+  this.value++;
 };
 
 console.log(myObject);
-
 myObject.incr();
 console.log(myObject);
-
 myObject.incr();
 console.log(myObject);
-
 myObject.incr();
 console.log(myObject);
